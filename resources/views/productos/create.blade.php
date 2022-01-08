f834d8c9f58354592fe2ac7cf27df7f5bb5d958d
Angel PCGM <angel.gomez.t89@gmail.com>
2022-01-07 22:54:59 -0600

    DEL No son necesarios

--- resources/views/productos/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.plantilla')
-
-@section('titulo','ALTA')
-
-@section('contenido')
-    <h1>Creaccion</h1>
-@endsection
--- resources/views/productos/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.plantilla')
-
-@section('titulo','Mostrar')
-
-@section('contenido')
-    <h1>Mostrar {{$producto}}</h1>
-@endsection
