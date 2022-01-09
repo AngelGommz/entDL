1a22be40a826bd1ce74830ad273a4aae67988501
Angel PCGM <angel.gomez.t89@gmail.com>
2022-01-09 13:14:06 -0600

    VIW NW Vista nueva para la Navegacion

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantilla')
+
+@section('titulo','Contacto')
+
+@section('contenido')
+    <div class="jumbotron mt-5">
+        <h1 class="display-4 text-center">Ing. Angel Alberto Gómez Tapia</h1>
+        <h4 class="text-center">Tel: 33 1486 - 13 - 23</h4>
+        <h4 class="text-center">Email: angel.gomez.t89@gmail.com</h4>        
+    </div>
+@endsection
